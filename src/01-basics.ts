740e9bd1af859e31caa13fa248031897fa660502
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2024-01-22 22:14:25 +0000

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -20,7 +20,16 @@ const friends = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1 = {
+interface Colleague {
+	name: string;
+	department: string;
+	contact: {
+		email: string;
+		extension: number;
+	};
+}
+
+const colleague1: Colleague = {
 	name: "Ralph Graham",
 	department: "Engineering",
 	contact: {
@@ -29,7 +38,7 @@ const colleague1 = {
 	},
 };
 
-const colleague2 = {
+const colleague2: Colleague = {
 	name: "Patti Burke",
 	department: "Finance",
 	contact: {
@@ -38,7 +47,7 @@ const colleague2 = {
 	},
 };
 
-const colleague3 = {
+const colleague3: Colleague = {
 	name: "Dean Sullivan",
 	department: "HR",
 	contact: {
